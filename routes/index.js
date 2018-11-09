499a660d1a047917d714e5834d1fed5d9c9a0ad4
KristenThesing <44411162+KristenThesing@users.noreply.github.com>
2018-11-09 11:51:07 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+var User = require('../models/user');
+var Claim = require('../models/claim');
+
 
 router.get('/',ensureAuthenticated, function(req,res){
      res.render('index');
@@ -15,4 +20,30 @@ function ensureAuthenticated(req,res,next){
 	}
 
 }
+
+var url = 'mongodb://localhost/loginapp';
+mongoose.connect('mongodb://localhost/loginapp');
+var db = mongoose.connection;
+var MongoClient = require('mongodb').MongoClient;
+router.get('/users/Review', function(req, res) {
+	 var username =req.body.username;
+     var password =req.body.password;
+     //var first_name = 'kristen';
+	User.getUserByUsername(username,function(err,user){
+   MongoClient.connect(url, function(err, db) {
+       var collection = db.collection('users');
+       var cursor = collection.find({});
+       str = "";
+       cursor.forEach(function(item) {
+           if (item != null) {
+                   str = str + "    Employee id  " + item.username + "</br>";
+           }
+       }, function(err) {
+           res.send(str);
+           db.close();
+          }
+       );
+   });
+ });
+});
 module.exports = router;
