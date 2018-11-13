f9a80b2206104b5fd17e647fe92ffb41d1613538
Taylor McLaughlin <44411229+tmclau96@users.noreply.github.com>
2018-11-13 10:32:47 -0500

    Update Activity.js

--- models/Activity.js
+++ models/Activity.js
@@ -2,7 +2,10 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcryptjs');
 
 var ActvitySchema = mongoose.Schema({
-
+  username:{
+		type: String,
+		index:true
+	},
 });
 
 var Activity = module.exports = mongoose.model('Activity',ActivitySchema)
