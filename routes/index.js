c1fdedfd9cfd8bff49f2a2d8fd7f8c6cc39ae13d
KristenThesing <44411162+KristenThesing@users.noreply.github.com>
2018-12-05 19:57:07 -0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ var mongoose = require('mongoose');
 var db = mongoose.connection;
 var User = require('../models/user');
 var Claim = require('../models/claim');
-var Admin = require('../models/Admin');
 
 
 router.get('/',ensureAuthenticated, function(req,res){
@@ -12,28 +11,13 @@ router.get('/',ensureAuthenticated, function(req,res){
 });
 
 
-router.get('/.',ensureAuthenticated1, function(req,res){
-     res.render('adminindex');
-});
-
 function ensureAuthenticated(req,res,next){
 	if(req.isAuthenticated()){
 		return next();
 
 	}else{
-		req.flash('error_msg','You are not logged in');
-		res.redirect('/users/login');
-	}
-
-}
-
-function ensureAuthenticated1(req,res,next){
-	if(req.isAuthenticated()){
-		return next();
-
-	}else{
 		//req.flash('error_msg','You are not logged in');
-		res.redirect('/users/Admin');
+		res.redirect('/users/login');
 	}
 
 }
@@ -42,25 +26,39 @@ var url = 'mongodb://localhost/loginapp';
 mongoose.connect('mongodb://localhost/loginapp');
 var db = mongoose.connection;
 var MongoClient = require('mongodb').MongoClient;
+
+
 router.post('/users/Activity', function(req, res) {
 	 var name1 =req.body.name;
    MongoClient.connect(url, function(err, db) {
        var collection = db.collection('claims');
        collection.find({first_name:name1}).toArray(function(err,db){
-       			res.send(db);
+       	//res.send("Claim1");
+       	res.send(db);
        });
    });
 });
-router.post('/users/ReviewClaims', function(req,res){
+var ObjectID = require('mongodb').ObjectID;
+router.post('/users/reviewClaims', function(req,res){
+	var claimID = req.body.claimId;
+	var status = req.body.status;
+	MongoClient.connect(url,function(err,db){
+		var collection = db.collection('claims');
+		collection.updateOne({_id: ObjectID(req.body.claimId)},{$set: {"claim_status" : status}});
+		collection.find().toArray(function(err,db){
+			res.send(db);
+			console.log(req.body.claimId);
+			console.log(status);
+		});
+	});
+});
+router.get('/users/reviewClaims', function(req,res){
 	MongoClient.connect(url,function(err,db){
 		var collection = db.collection('claims');
 		collection.find().toArray(function(err,db){
-			var str = db
-			res.send(str);
-
-
+			res.send(db);
 		});
-
 	});
 });
+
 module.exports = router;
